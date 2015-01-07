a8cc6441b773ad4a2865463b6ad0bd761b9bfddb
Michael Dao <michael.dao@gmail.com>
2015-01-07 10:16:57 -0700

    Person_id validating working

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe EmailAddress, :type => :model do
   let(:email_address) do
-    EmailAddress.new(address: "test@test.com")
+    EmailAddress.new(address: "test@test.com", person_id: 1)
   end
 
   it 'is valid' do
@@ -11,6 +11,11 @@ RSpec.describe EmailAddress, :type => :model do
 
   it 'is not valid without an address' do
     email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
+
+  it 'is not valid without a person id' do
+    email_address.person_id = nil
     expect(email_address).to_not be_valid
   end
 
